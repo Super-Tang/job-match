72f7e286e7d700a185f23b906067c12bf8d976f4
Super-Tang <36810099+Super-Tang@users.noreply.github.com>
2023-08-18 16:19:12 +0800

    Update step4_ensemble.py

--- step4_ensemble.py
+++ step4_ensemble.py
@@ -16,21 +16,24 @@ def cal_score(candidate):
     score += int(candidate['工作时间'][:-1])
     return score
 
+def sigmoid(x):
+    return 2*x/100
+
 def ensemble(result, max_count=5):
     ret = {}
     all_result = {}
     for i, key in enumerate(result.keys()):
         all_result['paraphrase-'+str(i)] = []
         scores = result[key]
-        for item in scores.keys():
+        for index, item in enumerate(scores.keys()):
             if item not in ret:
                 ret[item] = 0
-            elif i == 0: ret[item] = scores[item]
+            elif i == 0: ret[item] = scores[item] * (10-index)
             if i > 0:
-                ret[item] += scores[item] /(1+i)
-            if len(all_result) < 5: all_result['paraphrase-' + str(i)].append((item, scores[item]))
+                ret[item] += scores[item] / (2+2*i) * (10-index)
+            if len(all_result['paraphrase-' + str(i)]) < 5: all_result['paraphrase-' + str(i)].append((item, scores[item]))
     for item in ret:
-        ret[item] = math.exp(ret[item]) # * cal_score(item)
+        ret[item] = ret[item] * sigmoid(cal_score(item))
     ret = sorted(ret.items(), key=lambda x:x[1], reverse=True)
     all_result['final'] = ret[:max_count]
     return all_result
